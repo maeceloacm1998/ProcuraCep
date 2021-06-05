20ab834465de3048fe6f545330b1cf793bdb0145
Marcelo Antonio <maeceloacm1998@gmail.com>
2021-06-05 14:32:02 -0300

    Add API

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Alert, Text, View } from "react-native";
 
 import { ContainerSearchCep, TitleSearch, ContainerButton } from "./styled";
 
@@ -8,6 +8,24 @@ import Button from "./src/components/Button/Index";
 import api from "./src/services/api";
 
 export default function App() {
+  const [valueCep, setValueCep] = useState<string>("");
+
+  async function getCep(cep: any) {
+    try {
+      if (cep !== "") {
+        const { data } = await api.get(`${cep}/json`);
+
+        console.log(data);
+
+        return data;
+      } else {
+        Alert.alert("Digite um CEP valido! 😥");
+      }
+    } catch (error) {
+      Alert.alert("Error ao procurar o CEP 🤔, digite novamente...");
+    }
+  }
+
   return (
     <ContainerSearchCep>
       <TitleSearch>Hi User,</TitleSearch>
@@ -15,10 +33,19 @@ export default function App() {
         placeholder="Digite o cep"
         autoCorrect={false}
         keyboardType="numeric"
+        onChangeText={(value: string) => {
+          setValueCep(value);
+        }}
       />
       <ContainerButton>
         <Button style={{ marginRight: 15 }} color="#FB4C4B" children="Limpar" />
-        <Button color="#3ABEA6" children="Buscar" />
+        <Button
+          color="#3ABEA6"
+          children="Buscar"
+          onPress={() => {
+            getCep(valueCep);
+          }}
+        />
       </ContainerButton>
     </ContainerSearchCep>
   );
